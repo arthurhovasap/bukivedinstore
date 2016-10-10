214b5b866dfd3ebdfd9ee466636f77d1b76aa4db
a.ovasafyan <a.ovasafyan@IT04.BUKIVEDI.lan>
2016-10-10 11:56:06 +0300

    Bootstrap menu added!

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -46,21 +46,38 @@
 
     <div class="container-fluid" id="page" ng-controller="AppCtrl">
         <div class="row">
-            <div id="header">
-                    <div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
+            <div id="header" class="container-fluid">
+                <div class="row">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="navbar navbar-inverse">
+                                <div class="container-fluid">
+                                    <div class="navbar-header">
+                                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#gmenu">
+                                            <span class="sr-only">Open navigation</span>
+                                            <span class="icon-bar"></span>
+                                            <span class="icon-bar"></span>
+                                            <span class="icon-bar"></span>
+                                        </button>
+                                    </div>
+                                    <div class="collapse navbar-collapse" id="gmenu">
+                                        <?php $this->widget('zii.widgets.CMenu',array(
+                                                'htmlOptions'=>array('class'=>'nav navbar-nav'),
+                                                'items'=>array(
+                                                        array('label'=>'Home', 'url'=>array('/site/index')),
+                                                        array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
+                                                        array('label'=>'Contact', 'url'=>array('/site/contact')),
+                                                        array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
+                                                        array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
+                                                ),
+                                        )); ?>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div><!-- header -->
-
-            <div id="mainmenu">
-                    <?php $this->widget('zii.widgets.CMenu',array(
-                            'items'=>array(
-                                    array('label'=>'Home', 'url'=>array('/site/index')),
-                                    array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-                                    array('label'=>'Contact', 'url'=>array('/site/contact')),
-                                    array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
-                                    array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
-                            ),
-                    )); ?>
-            </div><!-- mainmenu -->
 
             <?php if(isset($this->breadcrumbs)):?>
                     <?php $this->widget('zii.widgets.CBreadcrumbs', array(
