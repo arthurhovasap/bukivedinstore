998b3ec08f7ad17013a10dbda1e251bca7ef5947
a.ovasafyan <a.ovasafyan@IT04.BUKIVEDI.lan>
2016-10-17 18:29:00 +0300

    17.10.2016 Заявки созданы, заказы созданы!!!

---
+++ protected/modules/stores/views/application/_form.php
@@ -0,0 +1,58 @@
+<?php
+/* @var $this ApplicationController */
+/* @var $model Application */
+/* @var $form CActiveForm */
+?>
+
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'application-form',
+	// Please note: When you enable ajax validation, make sure the corresponding
+	// controller action is handling ajax validation correctly.
+	// There is a call to performAjaxValidation() commented in generated controller code.
+	// See class documentation of CActiveForm for details on this.
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'name'); ?>
+		<?php echo $form->textField($model,'name',array('size'=>60,'maxlength'=>255)); ?>
+		<?php echo $form->error($model,'name'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'description'); ?>
+		<?php echo $form->textArea($model,'description',array('rows'=>6, 'cols'=>50)); ?>
+		<?php echo $form->error($model,'description'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'order_id'); ?>
+		<?php echo $form->textField($model,'order_id'); ?>
+		<?php echo $form->error($model,'order_id'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'count'); ?>
+		<?php echo $form->textField($model,'count'); ?>
+		<?php echo $form->error($model,'count'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'counttype_id'); ?>
+		<?php echo $form->textField($model,'counttype_id'); ?>
+		<?php echo $form->error($model,'counttype_id'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
---
+++ protected/modules/stores/views/application/_view.php
@@ -0,0 +1,33 @@
+<?php
+/* @var $this ApplicationController */
+/* @var $data Application */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('name')); ?>:</b>
+	<?php echo CHtml::encode($data->name); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('description')); ?>:</b>
+	<?php echo CHtml::encode($data->description); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('order_id')); ?>:</b>
+	<?php echo CHtml::encode($data->order_id); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('count')); ?>:</b>
+	<?php echo CHtml::encode($data->count); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('counttype_id')); ?>:</b>
+	<?php echo CHtml::encode($data->counttype_id); ?>
+	<br />
+
+
+</div>
