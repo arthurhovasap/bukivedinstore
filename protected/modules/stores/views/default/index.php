7f4934623436aa62b1bf967cfa0c40604ee3d19e
a.ovasafyan <a.ovasafyan@IT04.BUKIVEDI.lan>
2016-10-24 18:32:06 +0300

    24.10.2016 Заявки, первые шаги!

--- protected/modules/stores/views/default/index.php
+++ protected/modules/stores/views/default/index.php
@@ -17,3 +17,14 @@ $this->breadcrumbs=array(
 <p><b>Вопрос:</b> Как заказыскладов работают с заказами клиентов?<br>
 <b>Ответ:</b>Заказ на сырье производиться только после заказа клиента на основе потребности того или иного сырья, 
 кроме случаем, когда заказ делается с целью наполнения склада запасом.</p>
+
+<h2>Файлы из старого сайта которые перемещены и как то интегрированы с MVC Yii</h2>
+<ol>
+    <li><span class="text-info">modules/stores/order/</span><b class="text-warning">zakaz.php</b></li>
+    <li><span class="text-info">models/</span><b class="text-warning">Zakaz.php</b></li>
+    <li><span class="text-info">models/</span><b class="text-warning">Paper.php</b></li>
+    <li><span class="text-info">models/</span><b class="text-warning">Material.php</b></li>
+    <li><span class="text-info">models/</span><b class="text-warning">CalculationsParts.php</b></li>
+    <li><span class="text-info">models/</span><b class="text-warning">Calculations.php</b></li>
+    <li><span class="text-info">modules/stores/controllers/</span><b class="text-warning">OrderController.php</b></li>
+</ol>
